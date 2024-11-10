2f6513f07331556e1d9eaed785dc5ee123f4e4b8
Remy Johnny <ewerejohnnyjnr93@gmail.com>
2024-11-10 08:36:56 +0300

    implemented worker and the worklist

--- worker/worker.go
+++ worker/worker.go
@@ -1,1 +1,48 @@
 package worker
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type Result struct {
+	line string
+	lineNum int
+	path string
+}
+
+type Results struct {
+	inner []Result
+}
+
+func New(line string,lineNum int,path string) Result{
+	return Result{line,lineNum,path}
+}
+
+func searchFile(path string, key string) *Results {
+	file,err := os.Open(path)
+	if err != nil {
+		fmt.Println("Error : ", err)
+		return nil
+	}
+
+	results := Results{make([]Result, 0)}
+
+	scanner := bufio.NewScanner(file)
+	lineNum := 1
+	for scanner.Scan(){
+		if strings.Contains(scanner.Text(),key){
+			r := New(scanner.Text(),lineNum,path)
+			results.inner = append(results.inner, r)
+		}
+		lineNum++
+	}
+	if len(results.inner) == 0{
+		return nil
+	}else{
+		return &results
+	}
+
+}
--- worklist/worklist.go
+++ worklist/worklist.go
@@ -1,1 +1,39 @@
 package worklist
+
+type Entry struct{
+	path string
+}
+
+type WorkList struct{
+	jobs chan Entry
+}
+
+// Adds work to the channel
+func (w *WorkList) Add(work Entry){
+	w.jobs <- work
+}
+
+// Gets work out of the jobs channel
+func (w *WorkList) Next() Entry{
+	job := <- w.jobs
+	return job
+}
+
+// creates a new workList with a buffered jobs channel
+func New(bufSize int) WorkList{
+	return WorkList{
+		jobs: make(chan Entry,bufSize),
+	}
+}
+
+// Creates a new job i.e Entry
+func NewJob( path string) Entry{
+	return Entry{path}
+}
+
+// spams all the workers with a blank job (Entry). Indication for them to terminate
+func (w *WorkList) finalize(workersNum int ){
+	for i := 0; i < workersNum; i++{
+		w.Add(Entry{""})
+	}
+}
